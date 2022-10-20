5b657ef5079ba9428928e4ecddee754d86b05a4b
BORETS2002 <BORETS2002@example.com>
2022-10-20 08:42:04 +0300

    finish

---
+++ main.js
@@ -0,0 +1,41 @@
+const Elinput = document.querySelector(".form-input");
+const Elinputtwoo = document.querySelector(".form-inputtwoo");
+const Elinputhree = document.querySelector(".form-inputhree");
+const elBtn = document.querySelector(".btn");
+ 
+const ElBoxJavob =document.querySelector(".javob-box");
+ 
+
+
+elBtn.addEventListener("click", function (evt){
+
+  evt.preventDefault()
+  
+  arr = 
+  {
+  names: Elinput.value ,
+  Realitionship: Elinputtwoo.value ,
+  tel : Elinputhree.value
+  }
+  
+
+   Elli = document.createElement("li")
+  a = document.createElement("h3")
+  b =document.createElement("p")
+  c = document.createElement("a")
+   a.textContent = arr.names
+   b.textContent = arr.Realitionship
+   c.textContent = arr.tel
+ 
+   Elli.setAttribute ("class" , "itms")
+
+   Elli.appendChild(a)
+   Elli.appendChild(b)
+   Elli.appendChild(c)
+
+ElBoxJavob.appendChild(Elli)
+
+Elinput.value = ""
+Elinputtwoo.value = ""
+Elinputhree.value = ""
+})
